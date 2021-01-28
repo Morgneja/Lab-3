13d40f8064003f9aa342b538cfa02071b3e69039
Jason Morgner <morgneja@mail.uc.edu>
2021-01-28 18:09:01 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+class Main {
+  public static void main(String[] args) {
+    Scanner S = new Scanner(System.in);
+    int num;
+    System.out.println("Enter a number to count down or up from:");
+    num = S.nextInt();
+    if (num > 0)
+      for (int count = num; count >= 0; count--) {
+        System.out.println(count);
+      }
+    else
+      for (int count = num; count <= 0; count++) {
+        System.out.println(count);
+      }
+    System.out.println("Blast off!!!");
+  }
+}
